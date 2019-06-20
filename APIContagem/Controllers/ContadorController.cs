ddbe29477b1a85656a9477ddf5ea26d40069c015
yusuf_haydigo <josef@haydigo.com>
2019-06-20 11:29:41 +0300

    update

---
+++ APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIContagem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+
+        [HttpGet]
+        public object Get()
+        {
+            //http://localhost:10000/api/Contador/
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Sistema = Environment.OSVersion.VersionString
+                };
+            }
+        }
+    }
+}
